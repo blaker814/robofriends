4132cf99270f3de1aa627a94c5fc1934114f889d
blaker814 <54994930+blaker814@users.noreply.github.com>
2020-04-08 15:16:56 -0500

    moving to gitHub

---
+++ build/precache-manifest.e3f12734c09def6e1302f7ba4ba554e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e2af25db7d07611c872f4e620fad6a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b09fc992b9b017fee532",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "8741531b9899a0c90537",
+    "url": "/static/css/main.8a8e57b2.chunk.css"
+  },
+  {
+    "revision": "b09fc992b9b017fee532",
+    "url": "/static/js/2.0bc7f1f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0bc7f1f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8741531b9899a0c90537",
+    "url": "/static/js/main.6a441a9a.chunk.js"
+  },
+  {
+    "revision": "700cce3a7caf4f992d6c",
+    "url": "/static/js/runtime-main.ebe91ffd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
